ee154cbea5f52b7414b6e4d8864b956f8982cd88
lottamus <chrisnlott@gmail.com>
2021-10-20 09:55:46 -0500

    update sequences endpoint

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -2,20 +2,27 @@ import { createUser } from "../../lib/user";
 
 export default async function signup(req, res) {
   try {
-    const response = await fetch(`${process.env.AUTHMOJI_URL}/api/sequence/${req.body.phoneNumber}`, {
+    const response = await fetch(`${process.env.AUTHMOJI_URL}/api/sequences`, {
       method: "post",
       headers: {
         authorization: process.env.AUTHMOJI_SECRET,
+        "content-type": "application/json",
       },
+      body: JSON.stringify({
+        tel: req.body.phoneNumber,
+      }),
     });
 
     const data = await response.json();
     if (response.ok) {
-      const { sequenceId: verifyId } = data;
-  
+      const { sequence_id: verifyId } = data;
+
       await createUser({ ...req.body, verifyId });
-  
-      res.status(200).send({ done: true, verifyUrl: `${process.env.AUTHMOJI_URL}/verify/${verifyId}` });
+
+      res.status(200).send({
+        done: true,
+        verifyUrl: `${process.env.AUTHMOJI_URL}/verify/${verifyId}`,
+      });
     } else {
       res.status(400).end(data.message);
     }
